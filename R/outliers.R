919ed6c84675b6ea4afbc7e09f03115369940305
kapsner <lorenz.kapsner@web.de>
2019-07-29 13:14:54 +0200

    outlier handling, added flagOutlier function

--- R/outliers.R
+++ R/outliers.R
@@ -1,5 +1,5 @@
 # get list of cutoff values for outliers
-getNumOutliers <- function(vector){
+getOutlierLimits <- function(vector){
 
   # OutLo: Number of outliers (records below Q25-1.5*IQR)
   # OutHi: Number of outliers (records above Q75+1.5*IQR)
@@ -17,6 +17,26 @@ getNumOutliers <- function(vector){
   OutHi <- Q[2]+1.5*I
 
   return(list(lo=OutLo, hi=OutHi))
+}
+
+
+flagOutliers <- function(vector){
+  # OutLo: Number of outliers (records below Q25-1.5*IQR)
+  # OutHi: Number of outliers (records above Q75+1.5*IQR)
+  
+  # get quantiles of vector
+  Q <- stats::quantile(vector, probs=c(.25, .75), na.rm=T, names=F)
+  
+  # IQR
+  I <- stats::IQR(vector, na.rm=T)
+  
+  # OutLo: < Q25-1.5*IQR
+  OutLo <- Q[1]-1.5*I
+  
+  # OutHi: > Q75+1.5*IQR
+  OutHi <- Q[2]+1.5*I
+  
+  return(vector < OutLo | vector > OutHi)
 }
 
 # replace outliers
