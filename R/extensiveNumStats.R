4134b91042eacff49363ce6b2e8ffdea5abe1dd7
kapsner <lorenz.kapsner@web.de>
2019-11-03 14:51:59 +0100

    changed extensive_stats func

--- R/extensiveNumStats.R
+++ R/extensiveNumStats.R
@@ -1,38 +1,119 @@
-extensiveNumStats <- function(vector, rows=F, digits = 2){
+extensive_stats <- function(vector,
+                            rows = F,
+                            digits = 2) {
 
-  if (!is.null(vector)){
-    if (isTRUE(rows)){
+  if (!is.null(vector)) {
+    if (isTRUE(rows)) {
       vector <- unname(t(vector))
     }
 
     fac_vec <- factor(vector)
-    Q <- stats::quantile(vector, probs=c(.25, .75), na.rm=T, names=F)
-    I_out <- stats::IQR(vector, na.rm=T)*1.5
-
-    retdt <- list(N = as.numeric(base::sum(!is.na(vector))),
-                  "NA" = as.numeric(base::sum(is.na(vector))),
-                  Unique = as.numeric(base::nlevels(fac_vec)),
-                  Min = round(as.numeric(base::min(vector, na.rm = T)), digits),
-                  Q25 = round(as.numeric(Q[1]), digits),
-                  Q50 = round(as.numeric(stats::median(vector, na.rm = T)), digits),
-                  Mean = round(as.numeric(base::mean(vector, na.rm = T)), digits),
-                  Q75 = round(as.numeric(Q[2]), digits),
-                  Max = round(as.numeric(base::max(vector, na.rm = T)), digits),
-                  SD = round(as.numeric(stats::sd(vector, na.rm = T)), digits),
-                  Neg = as.numeric(base::sum(vector < 0, na.rm = T)),
-                  Zero = as.numeric(base::sum(vector == 0, na.rm = T)),
-                  Pos = as.numeric(base::sum(vector > 0, na.rm = T)),
-                  OutLo = as.numeric(base::sum(vector < (Q[1]-I_out), na.rm = T)),
-                  OutHi = as.numeric(base::sum(vector > (Q[2]+I_out), na.rm = T)),
-                  MAD_mean = round(as.numeric(stats::mad(vector, center = base::mean(vector), na.rm=T)), digits),
-                  MAD_med = round(as.numeric(stats::mad(vector, center = stats::median(vector), na.rm=T)), digits),
-                  Skeweness = round(as.numeric(e1071::skewness(vector, na.rm=T)), digits),
-                  Kurtosis = round(as.numeric(e1071::kurtosis(vector, na.rm=T)), digits),
-                  Variance = round(as.numeric(stats::var(vector, na.rm=T)), digits),
-                  Range = round(as.numeric(base::max(vector, na.rm=T) - base::min(vector, na.rm=T)), digits),
-                  IQR = round(as.numeric(stats::IQR(vector, na.rm=T)), digits),
-                  SE = round(as.numeric(se(vector)),  digits)
+    Q <- stats::quantile(
+      vector,
+      probs = c(.25, .75),
+      na.rm = T,
+      names = F
     )
+    I_out <- stats::IQR(
+      vector,
+      na.rm = T
+    ) * 1.5
+
+    retdt <- list(
+      N = as.numeric(base::sum(!is.na(vector))),
+      "NA" = as.numeric(base::sum(is.na(vector))),
+      Unique = as.numeric(base::nlevels(fac_vec))
+    )
+
+    if (is.numeric(vector)) {
+      retdt <- c(retdt,
+                 list(
+                   Min = round(
+                     as.numeric(base::min(vector, na.rm = T)),
+                     digits
+                   ),
+                   Q25 = round(
+                     as.numeric(Q[1]),
+                     digits
+                   ),
+                   Q50 = round(
+                     as.numeric(stats::median(vector, na.rm = T)),
+                     digits
+                   ),
+                   Mean = round(
+                     as.numeric(base::mean(vector, na.rm = T)),
+                     digits
+                   ),
+                   Q75 = round(
+                     as.numeric(Q[2]), digits
+                   ),
+                   Max = round(
+                     as.numeric(base::max(vector, na.rm = T)),
+                     digits
+                   ),
+                   SD = round(
+                     as.numeric(stats::sd(vector, na.rm = T)),
+                     digits
+                   ),
+                   Neg = as.numeric(
+                     base::sum(vector < 0, na.rm = T)
+                   ),
+                   Zero = as.numeric(
+                     base::sum(vector == 0, na.rm = T)
+                   ),
+                   Pos = as.numeric(
+                     base::sum(vector > 0, na.rm = T)
+                   ),
+                   OutLo = as.numeric(
+                     base::sum(vector < (Q[1] - I_out), na.rm = T)
+                   ),
+                   OutHi = as.numeric(
+                     base::sum(vector > (Q[2] + I_out), na.rm = T)
+                   ),
+                   MAD_mean = round(
+                     as.numeric(stats::mad(
+                       vector,
+                       center = base::mean(vector),
+                       na.rm = T)
+                     ),
+                     digits
+                   ),
+                   MAD_med = round(
+                     as.numeric(stats::mad(
+                       vector,
+                       center = stats::median(vector),
+                       na.rm = T)
+                     ),
+                     digits
+                   ),
+                   Skeweness = round(
+                     as.numeric(e1071::skewness(vector, na.rm = T)),
+                     digits
+                   ),
+                   Kurtosis = round(
+                     as.numeric(e1071::kurtosis(vector, na.rm = T)),
+                     digits
+                   ),
+                   Variance = round(
+                     as.numeric(stats::var(vector, na.rm=T)),
+                     digits
+                   ),
+                   Range = round(
+                     as.numeric(base::max(vector, na.rm = T) -
+                                  base::min(vector, na.rm = T)),
+                     digits
+                   ),
+                   IQR = round(
+                     as.numeric(stats::IQR(vector, na.rm = T)),
+                     digits
+                   ),
+                   SE = round(
+                     as.numeric(se(vector)),
+                     digits
+                   )
+                 )
+      )
+    }
   } else {
     # return empty table for initialization
     retdt <- data.table::data.table(
@@ -81,7 +162,7 @@ extensiveNumStats <- function(vector, rows=F, digits = 2){
 #'
 #' @export
 #'
-numStatsTable <- function(dataset, group_var = NULL, method = "all"){
+stats_table <- function(dataset, group_var = NULL, method = "all"){
 
   stopifnot(
     data.table::is.data.table(dataset),
@@ -89,28 +170,46 @@ numStatsTable <- function(dataset, group_var = NULL, method = "all"){
     ifelse(is.null(group_var), TRUE, is.character(group_var))
   )
 
-  # subset numeric values
-  vec <- colnames(dataset)[dataset[,sapply(.SD, is.numeric), .SDcols=colnames(dataset)]]
+  # subset numeric variables
+  #% vec <- colnames(dataset)[dataset[,sapply(.SD, is.numeric), .SDcols = colnames(dataset)]]
+
+  # subset all variables
+  vec <- colnames(dataset)
 
   stopifnot(
     length(vec) > 0
   )
 
   # init table
-  table <- extensiveNumStats(NULL)
+  table <- extensive_stats(NULL)
 
   if (is.null(group_var)){
     table <- cbind(Name = character(), table)
     for (var in vec){
-      table <- rbind(table, cbind(Name = var, dataset[,extensiveNumStats(get(var))]))
+      table <- data.table::rbindlist(
+        table,
+        cbind(Name = var, dataset[,extensive_stats(get(var))]),
+        fill = TRUE
+      )
     }
   } else {
     # TODO test for factor or character here
     table <- cbind(Name = character(), Group = character(), table)
     for (variable in vec){
-      table <- rbind(table, cbind(Name = var, Group = "", dataset[,extensiveNumStats(get(var))]))
+      table <- data.table::rbindlist(
+        table,
+        cbind(Name = var, Group = "", dataset[,extensive_stats(get(var))]),
+        fill = TRUE
+      )
       for (group in dataset[,unique(get(group_var))]){
-        table <- rbind(table, cbind(Name = "", Group = paste0("Group: ", group), dataset[get(group_var)==group,extensiveNumStats(get(variable))]))
+        table <- data.table::rbindlist(
+          table,
+          cbind(Name = "",
+                Group = paste0("Group: ", group),
+                dataset[get(group_var)==group,extensive_stats(get(variable))]
+          )
+          fill = TRUE
+        )
       }
     }
   }
@@ -142,7 +241,7 @@ numStatsTable <- function(dataset, group_var = NULL, method = "all"){
 #'
 #' @export
 #'
-numStatsTablePerCat <- function(dataset, var, group_var){
+stats_tablePerCat <- function(dataset, var, group_var){
 
   stopifnot(
     data.table::is.data.table(dataset),
@@ -152,7 +251,7 @@ numStatsTablePerCat <- function(dataset, var, group_var){
     is.factor(dataset[,get(group_var)])
   )
 
-  retdt <- extensiveNumStats(NULL)
+  retdt <- extensive_stats(NULL)
   outdat <- data.table::data.table(" " = colnames(retdt))
 
   cols <- ""
@@ -163,7 +262,7 @@ numStatsTablePerCat <- function(dataset, var, group_var){
 
     outdat <- cbind(outdat,
                     data.table::data.table(
-                      t(dataset[get(group_var)==cat,extensiveNumStats(get(var))])
+                      t(dataset[get(group_var)==cat,extensive_stats(get(var))])
                     ))
   }
   colnames(outdat) <- c(" ", paste("Category:", cols[2:length(cols)]))
--- tests/testthat/test-extensiveNumStats.R
+++ tests/testthat/test-extensiveNumStats.R
@@ -1,24 +1,24 @@
-context("test extensiveNumStats")
+context("test extensive_stats")
 
 prefix <- "./"
 #prefix <- "tests/testthat/"
 
-test_that("test extensiveNumStats", {
+test_that("test extensive_stats", {
   dataset <- data.table::data.table(datasets::mtcars)
   dataset[,("gear"):=factor(get("gear"))]
 
-  expect_silent(s1 <- dataset[,extensiveNumStats(get("wt"))])
+  expect_silent(s1 <- dataset[,extensive_stats(get("wt"))])
   expect_known_hash(s1, "2d98143852")
 })
 
 
-test_that("test numStatsTable", {
+test_that("test stats_table", {
   dataset <- data.table::data.table(datasets::mtcars)
   dataset[,("gear"):=factor(get("gear"))]
 
-  expect_silent(nt1 <- numStatsTable(dataset))
+  expect_silent(nt1 <- stats_table(dataset))
   expect_known_hash(nt1, "0edfd70a80")
 
-  expect_silent(nt2 <- numStatsTable(dataset, group_var = "gear"))
+  expect_silent(nt2 <- stats_table(dataset, group_var = "gear"))
   expect_known_hash(nt2, "fbd27dd015")
 })
